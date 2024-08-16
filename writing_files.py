7941d7857c8ae7d5a4fdf17791ddc39b58dbcbe7
piru <121550328+erpirulin@users.noreply.github.com>
2024-08-16 12:41:03 +0200

    writing_files.py

--- writing_files.py
+++ writing_files.py
@@ -1,1 +1,12 @@
-
+names = ["John", "Oscar", "Jacob"]
+ 
+file = open("names.txt", "w+")
+#escribir los nombres en el archivo
+for name in names:
+    files = name
+    print(files)
+file.close()
+ 
+file= open("names.txt", "r")
+#salida del contenido del archivo en la consola
+file.close()
